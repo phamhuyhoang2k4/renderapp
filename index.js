f1f96f3293adb4916eebde64eae8acf1983d543f
Hoàng <117429384+phamhuyhoang2k4@users.noreply.github.com>
2025-06-03 16:20:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+// 22dh114540 - Phạm Huy Hoàng
+
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Route chính trả về lời chào
 app.get("/", (req, res) => {
   res.send("Hello from Render!");
 });
 
+// Khởi động server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
